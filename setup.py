2927bb397418c6760922dc955e914335382ed5b0
tchayintr <t.chayintr@icloud.com>
2017-10-25 16:06:15 +0700

    v0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Thodsaporn Chay-intr',
     author_email='t.chayintr@icloud.com',
     url='https://github.com/tchayintr/postaggerTC',
-    packages=["postaggerTC", "artagger.InitialTagger", "artagger.SCRDRlearner", "artagger.Utility"],
+    packages=["postaggerTC", "postaggerTC.InitialTagger", "postaggerTC.SCRDRlearner", "postaggerTC.Utility"],
     include_package_data=True,
     license='MIT',
     zip_safe=False,
